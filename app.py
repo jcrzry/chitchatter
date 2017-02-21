236167bea4be6d2f7828d62d982a84187b157ce7
jcrzry <jcrzry@yahoo.com>
2017-02-21 20:25:13 +0000

    port update

--- app.py
+++ app.py
@@ -56,5 +56,7 @@ if __name__ == '__main__':
     socketio.run(
          app,
          host=os.getenv('IP', '0.0.0.0'),
-         port=int(os.getenv('PORT', 8080)),
+         port=int(os.getenv('PORT')),
          debug=True)
+         
+         
